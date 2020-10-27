aa81ef269f6ce090fb51dcc7a6c6b79a4aab9f30
Maël Querré <mael.querre@gmail.com>
2020-10-27 11:12:29 +0100

    Improve useLocalStorage hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useMemo, useReducer, useState } from 'react';
-
+import React, { useEffect, useMemo, useReducer } from 'react';
+import { useLocalStorage } from './hooks';
 import { fetchCocktail } from './domain/cocktails.services';
 import { default as CocktailReducer, initialState } from './domain/cocktails.reducer';
 
@@ -7,8 +7,8 @@ import CocktailCard from './components/CocktailCard';
 
 function App() {
   const [state, dispatch] = useReducer(CocktailReducer, initialState);
-  const [likedCocktailsIds, setLikedCocktailsIds] = useState([]);
-  const [skippedCocktailsIds, setSkippedCocktailsIds] = useState([]);
+  const [likedCocktailsIds, setLikedCocktailsIds] = useLocalStorage('likedCocktailsIds', []);
+  const [skippedCocktailsIds, setSkippedCocktailsIds] = useLocalStorage('skippedCocktailsIds', []);
 
   const likeCocktail = cocktailId => {
     setLikedCocktailsIds(likedCocktailsIds => [...likedCocktailsIds, cocktailId]);
---
+++ src/hooks/index.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      setStoredValue(value);
+
+      if (typeof value === 'function') {
+        value = value(storedValue);
+      }
+
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
